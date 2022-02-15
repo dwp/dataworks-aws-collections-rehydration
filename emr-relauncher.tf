63bfa70aca9cee5031917634a363bc54408be445
umair <muhammad.umair@engineering.digital.dwp.gov.uk>
2022-02-15 17:09:12 +0000

    EMR Relauncher updated, get secret for hive policy added

--- emr-relauncher.tf
+++ emr-relauncher.tf
@@ -7,10 +7,10 @@ variable "dataworks_emr_relauncher_zip" {
   }
 }
 
-resource "aws_lambda_function" "collections_rehydration_emr_relauncher" {
+resource "aws_lambda_function" "dataworks_aws_collections_rehydration_emr_relauncher" {
   filename      = "${var.dataworks_emr_relauncher_zip["base_path"]}/dataworks-emr-relauncher-${var.dataworks_emr_relauncher_zip["version"]}.zip"
-  function_name = "collections_rehydration_emr_relauncher"
-  role          = aws_iam_role.collections_rehydration_emr_relauncher_lambda_role.arn
+  function_name = "dataworks_aws_collections_rehydration_emr_relauncher"
+  role          = aws_iam_role.dataworks_aws_collections_rehydration_emr_relauncher_lambda_role.arn
   handler       = "event_handler.handler"
   runtime       = "python3.8"
   source_code_hash = filebase64sha256(
@@ -33,33 +33,33 @@ resource "aws_lambda_function" "collections_rehydration_emr_relauncher" {
   }
 
   tags = {
-    Name = "collections_rehydration_emr_relauncher"
+    Name = "dataworks_aws_collections_rehydration_emr_relauncher"
   }
 }
 
-resource "aws_cloudwatch_event_target" "collections_rehydration_emr_relauncher_target" {
+resource "aws_cloudwatch_event_target" "dataworks_aws_collections_rehydration_emr_relauncher_target" {
   rule      = aws_cloudwatch_event_rule.collections_rehydration_failed.name
-  target_id = "collections_rehydration_emr_relauncher_target"
-  arn       = aws_lambda_function.collections_rehydration_emr_relauncher.arn
+  target_id = "dataworks_aws_collections_rehydration_emr_relauncher_target"
+  arn       = aws_lambda_function.dataworks_aws_collections_rehydration_emr_relauncher.arn
 }
 
-resource "aws_iam_role" "collections_rehydration_emr_relauncher_lambda_role" {
-  name               = "collections_rehydration_emr_relauncher_lambda_role"
-  assume_role_policy = data.aws_iam_policy_document.collections_rehydration_emr_relauncher_assume_policy.json
+resource "aws_iam_role" "dataworks_aws_collections_rehydration_emr_relauncher_lambda_role" {
+  name               = "dataworks_aws_collections_rehydration_emr_relauncher_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.dataworks_aws_collections_rehydration_emr_relauncher_assume_policy.json
   tags = {
-    Name = "collections_rehydration_emr_relauncher_lambda_role"
+    Name = "dataworks_aws_collections_rehydration_emr_relauncher_lambda_role"
   }
 }
 
-resource "aws_lambda_permission" "collections_rehydration_emr_relauncher_invoke_permission" {
+resource "aws_lambda_permission" "dataworks_aws_collections_rehydration_emr_relauncher_invoke_permission" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.collections_rehydration_emr_relauncher.function_name
+  function_name = aws_lambda_function.dataworks_aws_collections_rehydration_emr_relauncher.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.collections_rehydration_failed.arn
 }
 
-data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_assume_policy" {
+data "aws_iam_policy_document" "dataworks_aws_collections_rehydration_emr_relauncher_assume_policy" {
   statement {
     sid     = "DataWorksCollectionsRehydrationEMRLauncherLambdaAssumeRolePolicy"
     effect  = "Allow"
@@ -72,7 +72,7 @@ data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_assume_po
   }
 }
 
-data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_scan_dynamo_policy" {
+data "aws_iam_policy_document" "dataworks_aws_collections_rehydration_emr_relauncher_scan_dynamo_policy" {
   statement {
     effect = "Allow"
 
@@ -87,7 +87,7 @@ data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_scan_dyna
   }
 }
 
-data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_sns_policy" {
+data "aws_iam_policy_document" "dataworks_aws_collections_rehydration_emr_relauncher_sns_policy" {
   statement {
     sid    = "AllowAccessToSNSLauncherTopic"
     effect = "Allow"
@@ -102,7 +102,7 @@ data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_sns_polic
   }
 }
 
-data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_pass_role_document" {
+data "aws_iam_policy_document" "dataworks_aws_collections_rehydration_emr_relauncher_pass_role_document" {
   statement {
     effect = "Allow"
     actions = [
@@ -114,50 +114,55 @@ data "aws_iam_policy_document" "collections_rehydration_emr_relauncher_pass_role
   }
 }
 
-resource "aws_iam_policy" "collections_rehydration_emr_relauncher_scan_dynamo_policy" {
+resource "aws_iam_policy" "dataworks_aws_collections_rehydration_emr_relauncher_scan_dynamo_policy" {
   name        = "DataWorksCollectionsRehydrationEmrRelauncherScanDynamoDb"
   description = "Allow Emr relauncher to scan pipeline metadata table"
-  policy      = data.aws_iam_policy_document.collections_rehydration_emr_relauncher_scan_dynamo_policy.json
+  policy      = data.aws_iam_policy_document.dataworks_aws_collections_rehydration_emr_relauncher_scan_dynamo_policy.json
   tags = {
-    Name = "collections_rehydration_emr_relauncher_scan_dynamo_policy"
+    Name = "dataworks_aws_collections_rehydration_emr_relauncher_scan_dynamo_policy"
   }
 }
 
-resource "aws_iam_policy" "collections_rehydration_emr_relauncher_sns_policy" {
+resource "aws_iam_policy" "dataworks_aws_collections_rehydration_emr_relauncher_sns_policy" {
   name        = "DataWorksCollectionsRehydrationEmrRelauncherSnsPublish"
   description = "Allow collections-rehydration to run job flow"
-  policy      = data.aws_iam_policy_document.collections_rehydration_emr_relauncher_sns_policy.json
+  policy      = data.aws_iam_policy_document.dataworks_aws_collections_rehydration_emr_relauncher_sns_policy.json
   tags = {
-    Name = "collections_rehydration_emr_relauncher_sns_policy"
+    Name = "dataworks_aws_collections_rehydration_emr_relauncher_sns_policy"
   }
 }
 
-resource "aws_iam_policy" "collections_rehydration_emr_relauncher_pass_role_policy" {
+resource "aws_iam_policy" "dataworks_aws_collections_rehydration_emr_relauncher_pass_role_policy" {
   name        = "DataWorksCollectionsRehydrationEmrRelauncherPassRole"
   description = "Allow Emr relauncher to publish messages to launcher topic"
-  policy      = data.aws_iam_policy_document.collections_rehydration_emr_relauncher_pass_role_document.json
+  policy      = data.aws_iam_policy_document.dataworks_aws_collections_rehydration_emr_relauncher_pass_role_document.json
   tags = {
-    Name = "collections_rehydration_emr_relauncher_pass_role_policy"
+    Name = "dataworks_aws_collections_rehydration_emr_relauncher_pass_role_policy"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "collections_rehydration_emr_relauncher_pass_role_attachment" {
-  role       = aws_iam_role.collections_rehydration_emr_relauncher_lambda_role.name
-  policy_arn = aws_iam_policy.collections_rehydration_emr_relauncher_pass_role_policy.arn
+resource "aws_iam_role_policy_attachment" "dataworks_aws_collections_rehydration_emr_relauncher_pass_role_attachment" {
+  role       = aws_iam_role.dataworks_aws_collections_rehydration_emr_relauncher_lambda_role.name
+  policy_arn = aws_iam_policy.dataworks_aws_collections_rehydration_emr_relauncher_pass_role_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "collections_rehydration_emr_relauncher_policy_execution" {
-  role       = aws_iam_role.collections_rehydration_emr_relauncher_lambda_role.name
+  role       = aws_iam_role.dataworks_aws_collections_rehydration_emr_relauncher_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "collections_rehydration_emr_relauncher_sns_attachment" {
-  role       = aws_iam_role.collections_rehydration_emr_relauncher_lambda_role.name
-  policy_arn = aws_iam_policy.collections_rehydration_emr_relauncher_sns_policy.arn
+resource "aws_iam_role_policy_attachment" "dataworks_aws_collections_rehydration_emr_relauncher_sns_attachment" {
+  role       = aws_iam_role.dataworks_aws_collections_rehydration_emr_relauncher_lambda_role.name
+  policy_arn = aws_iam_policy.dataworks_aws_collections_rehydration_emr_relauncher_sns_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "collections_rehydration_emr_relauncher_scan_dynamo_attachment" {
-  role       = aws_iam_role.collections_rehydration_emr_relauncher_lambda_role.name
-  policy_arn = aws_iam_policy.collections_rehydration_emr_relauncher_scan_dynamo_policy.arn
+resource "aws_iam_role_policy_attachment" "dataworks_aws_collections_rehydration_emr_relauncher_scan_dynamo_attachment" {
+  role       = aws_iam_role.dataworks_aws_collections_rehydration_emr_relauncher_lambda_role.name
+  policy_arn = aws_iam_policy.dataworks_aws_collections_rehydration_emr_relauncher_scan_dynamo_policy.arn
 }
 
+# Policy is defined in launcher
+resource "aws_iam_role_policy_attachment" "dataworks_aws_collections_rehydration_emr_relauncher_getsecrets" {
+  role       = aws_iam_role.dataworks_aws_collections_rehydration_emr_relauncher_lambda_role.name
+  policy_arn = aws_iam_policy.dataworks_aws_collections_rehydration_emr_launcher_getsecrets.arn
+}
